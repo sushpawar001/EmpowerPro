283644e946501b75c374bfbc049e0cf196c1647e
Sushant Pawar <sushpawar001@gmail.com>
2023-09-30 15:57:45 +0530

    Database settings

--- emp/emp/settings.py
+++ emp/emp/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'emp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'railway',
-        'USER': 'postgres',
-        'PASSWORD': 'lMlnL7i5bArGOt3mNfPh',
-        'HOST': 'containers-us-west-45.railway.app',
-        'PORT': '7152',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 

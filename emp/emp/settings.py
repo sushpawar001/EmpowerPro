d5a85911f22a175f81ab81cce73ac880a0658e6f
Sushant Pawar <sushpawar001@gmail.com>
2023-09-30 16:21:22 +0530

    Working Database

--- emp/emp/settings.py
+++ emp/emp/settings.py
@@ -77,14 +77,25 @@ WSGI_APPLICATION = 'emp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# DATABASES['default'] = dj_database_url.config()
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'lMlnL7i5bArGOt3mNfPh',
+        'HOST': 'containers-us-west-45.railway.app',
+        'PORT': 7152,
     }
 }
-
-DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
